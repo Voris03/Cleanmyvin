6ba2e07b39087f6be6646a66f92578247ac08807
Voris03 <v.vidus03@gmail.com>
2024-04-16 02:47:29 +0300

    fb: header & hero

--- src/hero/HeroOneButton.tsx
+++ src/hero/HeroOneButton.tsx
@@ -1,4 +1,6 @@
 import type { ReactNode } from 'react';
+import { Button } from './style';
+
 
 type IHeroOneButtonProps = {
   title: ReactNode;
@@ -8,12 +10,16 @@ type IHeroOneButtonProps = {
 
 const HeroOneButton = (props: IHeroOneButtonProps) => (
   <header className="text-center">
-    <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900">
+    <h1 className="whitespace-pre-line text-5xl font-bold leading-hero text-gray-900" style={{ wordSpacing: '1px' }}>
       {props.title}
     </h1>
     <div className="mb-16 mt-4 text-2xl">{props.description}</div>
 
-    {props.button}
+    <div style={{ color: "black" }}></div>  
+    
+    <Button>
+      {props.button}
+    </Button>
   </header>
 );
 
--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -11,14 +11,9 @@ const Hero = () => (
   <Background color="bg-gray-100">
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
-          </Link>
-        </li>
-        <li>
+        {/* <li>
           <Link href="/">Sign in</Link>
-        </li>
+        </li> */}
       </NavbarTwoColumns>
     </Section>
 
@@ -26,14 +21,14 @@ const Hero = () => (
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+              {'Купили авто из США и хотете знать с каким пробегом и в каком состоянии он был продан на аукционе?\n\n'}
+            {/* <span className="text-primary-500">React developers</span> */}
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="Cleanmyvin – лучшая база данных проданных авто на Copart и IAAI.Покупали авто на аукционах Copart и IAAI? Хотите узнать историю авто и все технические подробности?"
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
-            <Button xl>Download Your Free Theme</Button>
+          <Link href="/">
+            <Button xl>ПРОВЕРИТЬ АВТО</Button>
           </Link>
         }
       />
