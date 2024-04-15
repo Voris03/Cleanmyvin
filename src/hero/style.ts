92523399326f59adc1354e7c5c0178638bc0d3dd
Voris03 <v.vidus03@gmail.com>
2024-04-16 02:47:29 +0300

    fb: header & hero

---
+++ src/hero/style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const primaryColor = "rgb(3, 169, 244)";
+const hoverColor = "rgb(247 250 252)";
+
+export const Button = styled("div")`
+    background-color: ${primaryColor};
+    border: none;
+    color: ${hoverColor};
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 10px;
+    transition-duration: 0.4s;
+
+    &:hover {
+        background-color: ${hoverColor};
+        border: 2px solid ${primaryColor};
+        color: black;
+    }
+
+    &:focus {
+        outline: none;
+    }
+`;
+
+
--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
+  site_name: 'Cleanmyvin',
+  title: 'Cleanmyvin',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
-  locale: 'en',
+  locale: 'ru',
 };
