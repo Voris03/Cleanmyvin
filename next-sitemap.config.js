44b24a789b01a21555c3c6221917d470b1bfaffa
Denis Muraveiko <d.muraveiko@leenda.com>
2024-04-28 23:22:41 +0300

    init

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://checkusavin.com",
+  generateRobotsTxt: true,
+  sitemapSize: 50_000,
+  changefreq: "hourly",
+};
