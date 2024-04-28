2bc03b946fd1e00e5793bfe984d5147f9a9ef9e0
Denis Muraveiko <d.muraveiko@leenda.com>
2024-04-29 02:18:38 +0300

    init

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,11 @@ module.exports = {
   siteUrl: "https://checkusavin.com",
   generateRobotsTxt: true,
   sitemapSize: 50_000,
-  changefreq: "hourly",
+  exclude: ["/payment/*", "/server-sitemap-index.xml", "/server-sitemap.xml"],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      "https://checkusavin.com/server-sitemap-index.xml",
+      "https://checkusavin.com/server-sitemap.xml",
+    ],
+  },
 };
