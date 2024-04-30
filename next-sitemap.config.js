203a12dba742f5f8607762c5916c74c6e3536bea
Denis Muraveiko <d.muraveiko@leenda.com>
2024-04-30 18:12:01 +0300

    comments and tasks

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://checkusavin.com",
-  generateRobotsTxt: true,
-  sitemapSize: 10_000,
-  exclude: ["/payment/*"],
-};
